e21011189ef9d14a53b0ea007cff076c98236b92
KhangHg <nghia.hnkt@gmail.com>
2023-10-04 16:34:53 +0700

    create image dice

--- app/src/main/java/com/example/diceroller/ui/theme/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/ui/theme/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller.ui.theme
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.diceroller.R
 
@@ -12,17 +12,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            rollDice(R.id.textView)
-            rollDice(R.id.textView2)
-            
+//            rollDice(R.id.textView)
+//            rollDice(R.id.textView2)
+            rollDice()
         }
+
+        rollDice()
     }
 
-    private fun rollDice(idTextView:Int) {
+    private fun rollDice() {
         val dice = Dice(6)
-        val diceRol = dice.roll()
-        val  resultTextView: TextView = findViewById(idTextView)
-        resultTextView.text = diceRol.toString()
+        val diceRoll = dice.roll()
+//        val  resultTextView: TextView = findViewById(idTextView)
+//        resultTextView.text = diceRol.toString()
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+        diceImage.contentDescription = diceRoll.toString()
     }
 
     class Dice(val numSides: Int){
